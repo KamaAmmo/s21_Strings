b573901fda38bc3e77dba63fcc0950ada51eaf03
Kamil <kamilggss@gmail.com>
2023-07-01 01:29:40 +0300

    strchr

--- src/funcs/s21_strchr.c
+++ src/funcs/s21_strchr.c
@@ -1,3 +1,8 @@
 #include "../s21_string.h"
 
-char *s21_strchr(const char *str, int c) { return NULL; }
+char *s21_strchr(const char *str, int c) {
+    char *res = s21_NULL;
+    char *src = (char *)str;
+    while (*src++) if (*src == c) res = src; 
+    return res;
+}
