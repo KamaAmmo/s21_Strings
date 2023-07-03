854a0bb5d5f8563b91bd48a7fa7ee7e4915e698f
Lorraine Emory <lorraine@student.21-school.ru>
2023-07-03 14:26:35 +0000

    Update s21_strrchr.c

--- src/funcs/s21_strrchr.c
+++ src/funcs/s21_strrchr.c
@@ -3,11 +3,12 @@
 //находит последнее вхождение символа в строку
 
 char* s21_strrchr(const char* str, int c) {
-  char* search = NULL;
-  char* p = (char*)str;
-  while (*p != '\0') {
-    if (*p == c) search = p;
-    p++;
-  }
-  return search;
+    char* search = s21_NULL;
+    char* p = (char*)str;
+    while (*p != '\0') {
+        if (*p == c)
+            search = p;
+        p++;
+    }
+    return search;
 }
