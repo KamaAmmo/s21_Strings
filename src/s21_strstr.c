b37ad7f6f4f3acdd36c033bfd71f542a7de2e823
Lorraine Emory <lorraine@student.21-school.ru>
2023-07-03 15:44:01 +0000

    Update s21_strstr.c

--- src/s21_strstr.c
+++ src/s21_strstr.c
@@ -1,25 +1,25 @@
 #include "../s21_string.h"
 
-//первое вхождение подстроки в строку
-
 char *s21_strstr(const char *haystack, const char *needle) {
-  char *search = S21_NULL;
-  char *hay = (char*)haystack;
-  for (; *hay != '\0'; hay++) {
-    if (!search) {
-      if (*hay == *needle) {
-        search = hay;
-        char* nd = (char*)needle;
-        for (; *nd != '\0'; nd++, hay++) {
-          if (*hay == '\0') {
-            search = S21_NULL;
-            break;
-          }
-          if (*hay != *nd)
-              search = S21_NULL;
+    char *search = s21_NULL;
+    char *hay = (char*)haystack;
+    if (*needle == '\0')
+        search = (char*)haystack;
+    for (; *hay != '\0'; hay++) {
+        if (!search) {
+            if (*hay == *needle) {
+                search = hay;
+                char* nd = (char*)needle;
+                for (; *nd != '\0'; nd++, hay++) {
+                    if (*hay == '\0') {
+                        search = s21_NULL;
+                        break;
+                    }
+                    if (*hay != *nd)
+                        search = s21_NULL;
+                }
+            }
         }
-      }
     }
-  }
-  return search;
+    return search;
 }
