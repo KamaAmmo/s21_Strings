2760c20b7b184c13fbdf739bf0fb465afc32ea01
Lorraine Emory <lorraine@student.21-school.ru>
2023-07-03 14:41:22 +0000

    lorraine (fixed '\0')

--- src/funcs/s21_strrchr.c
+++ src/funcs/s21_strrchr.c
@@ -6,9 +6,12 @@ char* s21_strrchr(const char* str, int c) {
     char* search = s21_NULL;
     char* p = (char*)str;
     while (*p != '\0') {
-        if (*p == c)
+        if (*p == c) {
             search = p;
+        }
         p++;
     }
+    if (c == '\0')
+        search = "";
     return search;
 }
