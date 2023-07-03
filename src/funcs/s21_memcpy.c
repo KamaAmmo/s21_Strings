8a690e514b8db6f4bbd75e23a76f65d30a615479
Suzy Margaretta <suzymarg@mi-j6.msk.21-school.ru>
2023-07-03 16:27:29 +0300

    Tests_v1

--- src/funcs/s21_memcpy.c
+++ src/funcs/s21_memcpy.c
@@ -4,4 +4,5 @@ void *s21_memcpy(void *dest, const void *src, size_t n) {
   char *destination = (char *)dest;
   char *source = (char *)src;
   for (int i = 0; (i < n); i++) destination[i] = source[i];
+  return destination;
 }
--- src/test-suites/suite_strchr.c
+++ src/test-suites/suite_strchr.c
@@ -1,9 +1,35 @@
 #include "../s21_string_tests.h"
 
+START_TEST(test_strchr_1) {
+  char example1[256] = "sdf FFF UWEuw 0";
+  int c = '0';
+  ck_assert_str_eq(strchr(example1, c), s21_strchr(example1, c));
+}
+END_TEST
+
+START_TEST(test_strchr_2)  // !!!!!!!!!!!!!!!!!!!!!
+{
+  char example1[256] = "JJ sv'av w \0 ds s";
+  int c = '\0';
+  ck_assert_str_eq(strchr(example1, c), s21_strchr(example1, c));
+}
+END_TEST
+
+START_TEST(test_strchr_3) {
+  char example1[256] = "JJ sv'av w \0 ds s";
+  int c = 'r';
+  ck_assert_ptr_eq(strchr(example1, c), s21_strchr(example1, c));
+}
+END_TEST
+
 Suite *suite_strchr() {
   Suite *s = suite_create("s21_strchr");
 
-  TCase *tc_core = tcase_create("");
+  TCase *tc_core = tcase_create("t_case_strchr");
+
+  tcase_add_test(tc_core, test_strchr_1);
+  tcase_add_test(tc_core, test_strchr_2);
+  tcase_add_test(tc_core, test_strchr_3);
   suite_add_tcase(s, tc_core);
 
   return s;
