396bd38c4ea333d88af181874f780a8454add53c
Kamil <kamilggss@gmail.com>
2023-07-04 23:37:37 +0300

    Fixed strncmp

--- src/funcs/s21_strncmp.c
+++ src/funcs/s21_strncmp.c
@@ -2,10 +2,12 @@
 
 int s21_strncmp(
     const char *str1, const char *str2,
-    s21_size_t n) {  //отличие от memcmp: memcmp пофиг на /0, => strncmp is better
-  int res = 0;
+    s21_size_t n) {
   const char *p1 = (const char *)str1;
   const char *p2 = (const char *)str2;
-  for (int i = 0; (i < n) && p1[i] && p2[i] && !res; i++) res = p1[i] - p2[i];
-  return res;
-}
+  int res = 0;
+  for (int i = 0; (i < n) && !res; i++) {
+    res = p1[i] - p2[i];
+    if (!p1[i] || !p2[i]) break;
+    }
+}     
