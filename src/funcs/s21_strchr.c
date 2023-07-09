53e03a473360bc9e5c079e7d5067959a998f3319
Diann Norah <diannnor@student.21-school.ru>
2023-07-09 15:08:29 +0000

    Update s21_strchr.c

--- src/funcs/s21_strchr.c
+++ src/funcs/s21_strchr.c
@@ -1,12 +1,11 @@
 #include "../s21_string.h"
 
 char *s21_strchr(const char *str, int c) {
-  char *res = s21_NULL;
+  char *res = NULL;
   char *src = (char *)str;
   while (*src && !res) {
     if (*src == c) res = src;
     src++;
   }
-  if (c == '\0') res = "";
   return res;
 }
