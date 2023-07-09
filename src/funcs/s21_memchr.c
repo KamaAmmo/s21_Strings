b90925fcd1817c7b4c1bda6929a4ecb4a4a3a8d6
Diann Norah <diannnor@student.21-school.ru>
2023-07-09 15:09:05 +0000

    Update s21_memchr.c

--- src/funcs/s21_memchr.c
+++ src/funcs/s21_memchr.c
@@ -1,10 +1,10 @@
 #include "../s21_string.h"
 
-void *s21_memchr(const void *str, int c, s21_size_t n) {
-  void *res = s21_NULL;
+void *s21_memchr(const void *str, int c, size_t n) {
+  char *res = NULL;
   const unsigned char *ptr = (const unsigned char *)str;
-  for (int i = 0; (i <= n) && !res; i++) {
-    if (ptr[i] == c) res = (void *)&ptr[i];
+  for (int i = 0; (i < n) && !res; i++) {
+    if (ptr[i] == c) res = &ptr[i];
   }
   return res;
 }
