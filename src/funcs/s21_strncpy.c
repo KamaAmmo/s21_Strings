aff551b0511d0095aededf62c5d65cd63db7c1cd
Diann Norah <diannnor@student.21-school.ru>
2023-07-09 16:43:44 +0000

    Update s21_strncpy.c

--- src/funcs/s21_strncpy.c
+++ src/funcs/s21_strncpy.c
@@ -1,12 +1,12 @@
 #include "../s21_string.h"
 
-char *s21_strncpy(char *dest, const char *src, s21_size_t n) {
-  char *destination = (char *)dest;
-  const char *source = (char *)src;
-  for (s21_size_t i = 0; i < n; i++, destination++, source++) {
-    if (!*destination || !*source) break;
-    *destination = *source;
+char *s21_strncpy(char *dest, const char *src, size_t n) {
+  size_t i = 0;
+  for (; src[i] && (i < n); i++){
+    dest[i] = src[i];
   }
-  *destination = '\0';
+  for (; i < n; i++){
+    dest[i] = '\0';
+  }
   return dest;
 }
