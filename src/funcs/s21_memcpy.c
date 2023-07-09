c8c1f383f5a429de193fb092e266d73b9c3ea4df
Diann Norah <diannnor@student.21-school.ru>
2023-07-09 15:05:38 +0000

    Update s21_memcpy.c

--- src/funcs/s21_memcpy.c
+++ src/funcs/s21_memcpy.c
@@ -1,8 +1,12 @@
 #include "../s21_string.h"
 
-void *s21_memcpy(void *dest, const void *src, s21_size_t n) {
+void *s21_memcpy(void *dest, const void *src, size_t n) {
   char *destination = (char *)dest;
-  char *source = (char *)src;
-  for (int i = 0; (i < n); i++) destination[i] = source[i];
-  return destination;
+  const char *source = (char *)src;
+  for (size_t i = 0; i < n; i++){
+    *destination = *source;
+    destination++;
+    source++;
+  }
+  return dest;
 }
