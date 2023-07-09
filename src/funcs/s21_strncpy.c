111b32e19c533397a260ce733e2e42c153fe3350
morganma(Roumaan) <morganma@student.21-school.ru>
2023-07-09 19:48:40 +0300

    replace size_t

--- src/funcs/s21_strncpy.c
+++ src/funcs/s21_strncpy.c
@@ -1,11 +1,11 @@
 #include "../s21_string.h"
 
-char *s21_strncpy(char *dest, const char *src, size_t n) {
-  size_t i = 0;
-  for (; src[i] && (i < n); i++){
+char *s21_strncpy(char *dest, const char *src, s21_size_t n) {
+  s21_size_t i = 0;
+  for (; src[i] && (i < n); i++) {
     dest[i] = src[i];
   }
-  for (; i < n; i++){
+  for (; i < n; i++) {
     dest[i] = '\0';
   }
   return dest;
