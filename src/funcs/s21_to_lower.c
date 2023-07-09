52117d77e3905865c0407eaebb77ac4c20091cd8
Diann Norah <diannnor@student.21-school.ru>
2023-07-09 15:04:01 +0000

    Update s21_to_lower.c

--- src/funcs/s21_to_lower.c
+++ src/funcs/s21_to_lower.c
@@ -1,16 +1,15 @@
 #include "../s21_string.h"
 
 void *s21_to_lower(const char *str) {
-  char *tmp = s21_NULL;
+  char * tmp = NULL;
   if (str) {
     size_t len = s21_strlen(str);
-    tmp = malloc((len) * sizeof(char));
+    tmp = malloc((len + 1) * sizeof(char));
     for (int i = 0; i < len; i++) {
-      if (str[i] >= 'A' && str[i] <= 'Z')
-        tmp[i] = str[i] + ('a' - 'A');
-      else
-        tmp[i] = str[i];
+      if (str[i] >= 'A' && str[i] <= 'Z') tmp[i] = str[i] + ('a' - 'A');
+      else tmp[i] = str[i];
     }
+    tmp[len] = '\0';
   }
-  return tmp;
+  return (void *)tmp;
 }
