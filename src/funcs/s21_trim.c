468306dd481d2991cdb407f78bd97a576591f707
Kamil <kamilggss@gmail.com>
2023-07-10 22:09:49 +0300

    fix trim + tests

--- src/funcs/s21_trim.c
+++ src/funcs/s21_trim.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 
 #include "../s21_string.h"
+
 int char_in_trim(char a, const char *trim_chars, s21_size_t len_trim) {
   for (s21_size_t i = 0; i < len_trim; i++)
     if (trim_chars[i] == a) return 1;
@@ -33,12 +34,18 @@ void *s21_trim(const char *src, const char *trim_chars) {
   char *tmp = S21_NULL;
   s21_size_t len1 = s21_strlen(src);
   s21_size_t len2 = s21_strlen(trim_chars);
+
   int t = 0;
   if (src) {
     tmp = malloc((len1 + 1) * sizeof(char));
     int cbegin = 0, cend = 0;
-    check_begin_src(src, trim_chars, &cbegin);
-    check_end_src(src, trim_chars, len1, len2, &cend);
+    if (len2 == 0) {
+      check_begin_src(src, " ", &cbegin);
+      check_end_src(src, " ", len1, 1, &cend);
+    } else {
+      check_begin_src(src, trim_chars, &cbegin);
+      check_end_src(src, trim_chars, len1, len2, &cend);
+    }
 
     for (s21_size_t i = cbegin; i < len1 - cend; i++) {
       tmp[t] = src[i];
