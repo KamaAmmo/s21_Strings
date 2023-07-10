e3ff53921e974e56b77b378d0415216872f7f397
artem <artem@artem>
2023-07-10 12:44:33 +0300

    Fix trim

--- src/funcs/s21_trim.c
+++ src/funcs/s21_trim.c
@@ -2,11 +2,6 @@
 
 #include "../s21_string.h"
 
-// int check_end_src(const char *src, const char *trim_chars, s21_size_t len1,
-//                   s21_size_t len2);
-// int check_begin_src(const char *src, const char *trim_chars, s21_size_t
-// len2);
-
 void check_begin_src(const char *src, const char *trim_chars, int *c) {
   int j = 0;
   int i = 0;
@@ -38,16 +33,18 @@ void *s21_trim(const char *src, const char *trim_chars) {
   char *tmp = S21_NULL;
   s21_size_t len1 = s21_strlen(src);
   s21_size_t len2 = s21_strlen(trim_chars);
+  int t = 0;
   if (src) {
-    tmp = malloc((len1) * sizeof(char));
+    tmp = malloc((len1 + 1) * sizeof(char));
     int cbegin = 0, cend = 0;
     check_begin_src(src, trim_chars, &cbegin);
     check_end_src(src, trim_chars, len1, len2, &cend);
-    int t = 0;
+
     for (s21_size_t i = len2 * cbegin; i < len1 - (len2 * cend); i++) {
       tmp[t] = src[i];
       t++;
     }
+    tmp[t] = '\0';
   }
-  return tmp;
+  return (void *)tmp;
 }
